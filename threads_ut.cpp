a8386499cbb0d6c3ca8dfd642d92b520b3305b08
Davy Souza <davy.souza@outlook.com>
2021-04-18 12:51:16 -0300

    added geekforgeeks example of multithreading

--- threads_ut.cpp
+++ threads_ut.cpp
@@ -38,7 +38,26 @@ void count1m (int id) {
     cout << "    (results): thread #" << id << " won!\n";
 }
 
-// main functions
+// A dummy function
+void foo(int Z) {
+  for (int i = 0; i < Z; i++)
+    cout << "    (function pointer): Thread using function pointer as callable\n";
+  cout << endl;
+}
+
+// A callable object
+class thread_obj {
+public:
+  void operator() (int x) {
+    for (int i = 0; i < x; i++)
+      cout << "    (function object): Thread using function object as callable\n";
+    cout << endl;
+  }
+};
+
+// 
+// ========== main functions ==========
+// 
 
 void thread_examples(void) {
   thread t1(thread_func, 2, 'a');
@@ -67,15 +86,53 @@ void atomic_examples(void) {
     th.join();
 }
 
+// https://www.geeksforgeeks.org/multithreading-in-cpp/
+void geekforgeeks_examples(void) {
+  cout << "    (threads 1 and 2 and 3 operating independently)\n\n";
+  
+  // This thread is launched by using 
+  // function pointer as callable
+  thread th1(foo, 3);
+  
+  // This thread is launched by using
+  // function object as callable
+  thread th2(thread_obj(), 3);
+  
+  // Define a Lambda Expression
+  auto f = [](int x) {
+    for (int i = 0; i < x; i++)
+      cout << "    (lambda expression): Thread using lambda expression as callable\n";
+    cout << endl;
+  };
+  
+  // This thread is launched by using 
+  // lamda expression as callable
+  thread th3(f, 3);
+
+  // Wait for the threads to finish
+  // Wait for thread t1 to finish
+  th1.join();
+
+  // Wait for thread t2 to finish
+  th2.join();
+
+  // Wait for thread t3 to finish
+  th3.join();
+}
+
 int main() {
   cout << "\n========== MULTITHREADING EXAMPLES ==========\n\n";
 
-  cout << "[thread]\n";;
+  cout << "[thread]\n";
   thread_examples();
   cout << "\n    done!\n\n";
 
-  cout << "[atomic]\n";;
+  cout << "[atomic]\n";
   atomic_examples();
+  cout << "\n    done!\n\n";
+
+  cout << "[geekforgeeks example]\n";
+  geekforgeeks_examples();
   cout << "\n    done!\n\n";
 
   return 0;
